9ce4aacb2f31c5fca0ea9d1da86892f450601dc6
Ethan Strominger <ethanstrominger2@gmail.com>
2020-05-30 07:21:20 -0400

    Add get and add controller tests

--- src/test/java/com/ethan/controller/GameControllerTest.java
+++ src/test/java/com/ethan/controller/GameControllerTest.java
@@ -1,4 +1,37 @@
 package com.ethan.controller;
 
+import com.ethan.GameMessage;
+import com.ethan.GameMessageController;
+import com.ethan.GameMessages;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameControllerTest {
+    @Test
+    public void GameAddControllerReturnsCorrectBodyAndStatus() {
+        GameMessage gameMessage = new GameMessage("a","b","c");
+        ResponseEntity<GameMessage> r =
+                GameMessageController.controllerAddMessage((gameMessage));
+
+        assertEquals(r.getStatusCode(), HttpStatus.OK);
+        GameMessage resultMessage = r.getBody();
+        assertEquals(gameMessage.getId(), resultMessage.getId());
+    }
+
+    @Test
+    public void GameGetMessagesReturnsCorrectBodyAndStatus() {
+        final String userName = "Ethan";
+        GameMessage gameMessage = new GameMessage(userName,"b","c");
+        GameMessages.addMessage(gameMessage);
+
+        ResponseEntity<GameMessage[]> r =
+                GameMessageController.getMessages(userName);
+
+        assertEquals(r.getStatusCode(), HttpStatus.OK);
+        GameMessage[] resultMessage = r.getBody();
+        assertEquals(resultMessage.length, GameMessages.getMessages(userName).length);
+    }
 }
